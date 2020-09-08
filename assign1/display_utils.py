67c41c9064a52f68a6237a2ff9ac8958e38743b0
Amanda Adkins <mandi1267@gmail.com>
2020-09-08 15:57:59 -0500

    Continue cleaning up assignment 1. Add display of amount of time it takes to find eigen decomposition by training set size. Also add reconstructed image from 25-feature representation and 784-feature representation

--- assign1/display_utils.py
+++ assign1/display_utils.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def convertVectorToImage(image_vector, num_rows, num_columns):
@@ -33,3 +34,57 @@ def displayImage(image_data, true_label=None, classified_label=None):
     plt.title(title_str)
     plt.imshow(image_data, interpolation='none')
     plt.show()
+
+def displayEigenVector(num_rows_in_img, num_cols_in_img, eigen_vec):
+    """
+    Display the eigenvector as an image.
+
+    Args:
+        num_rows_in_img (int):      Number of rows in images.
+        num_cols_in_img (int):      Number of columns in images.
+        eigen_vec (1D numpy array): Eigenvector to display as an image.
+    """
+    eigen_vector_matrix = convertVectorToImage(eigen_vec, num_rows_in_img, num_cols_in_img)
+    plt.title("Eigen vector")
+    plt.imshow(eigen_vector_matrix, interpolation='none')
+    plt.show()
+
+
+def displayEigenVectorRepresentationOfImage(eigen_vec_img_representation, eigen_vector_matrix, num_rows_in_orig_img,
+    num_cols_in_orig_img, mean_feature_vec):
+    """
+    Reconstruct and display an image from the eigenvector-space representation of the image and the eigenvector matrix
+    and mean value of the pixels from which the eigenvectors were computed.
+
+    Args:
+        eigen_vec_img_representation (1D Numpy array):  Eigenvector-space representation of an image (reduced-feature image representation).
+        eigen_vector_matrix (2D Numpy array):           N x M matrix with the top M eigenvectors of the training data as columns.
+        num_rows_in_orig_img (int):                     Number of rows in the original image format.
+        num_cols_in_orig_img (int):                     Number of columns in the original image format.
+        mean_feature_vec (1D Numpy array):              Mean value for each pixel in the original training data set.
+    """
+
+    eig_pseudo_inv = np.linalg.pinv(eigen_vector_matrix)
+    expanded_img_vec = (eigen_vec_img_representation @ eig_pseudo_inv) + mean_feature_vec
+    eigen_vector_matrix = convertVectorToImage(expanded_img_vec, num_rows_in_orig_img, num_cols_in_orig_img)
+    plt.title("Reconstructed image from eigenvector representation with " + str(eigen_vec_img_representation.size) + " features")
+    plt.imshow(eigen_vector_matrix, interpolation='none')
+    plt.show()
+
+def plotGraph(x_values, y_values, x_axis_label, y_axis_label, graph_title):
+    """
+    Plot a graph with the given data.
+
+    Args:
+        x_values (1D array):    X values to plot
+        y_values (1D array):    Y values to plot. Should have same number of items as x_values
+        x_axis_label (string):  Label for the x axis
+        y_axis_label (string):  Label for the y axis
+        graph_title (string):   Title for the graph
+    """
+    plt.plot(x_values, y_values, 'rD-.')
+    plt.xlabel(x_axis_label)
+    plt.ylabel(y_axis_label)
+    plt.title(graph_title)
+    plt.show()
+    # TODO verify this
